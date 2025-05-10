f8d47ad6ec9a75b3156b3530179b3ddaee199eba
hamady2026 <hamady106lethe@gmail.com>
2025-05-10 22:46:22 +0300

    تم انشاء دالة ترجع بقائمة من اصحاب العقارات والقائمة بها 2من اصحاب العقارات كتصميم اولي

--- Project2025/Components/Pages/PropertyOwnerComponents/PropertyOwnerDesignServices.cs
+++ Project2025/Components/Pages/PropertyOwnerComponents/PropertyOwnerDesignServices.cs
@@ -1,6 +1,26 @@
-﻿namespace Project2025.Components.Pages.PropertyOwnerComponents
+﻿using Project2025.Data.Entity;
+
+namespace Project2025.Components.Pages.PropertyOwnerComponents
 {
     public class PropertyOwnerDesignServices
     {
+        public List<PropertyOwner> GetPropertyOwners()
+        {
+            return new List<PropertyOwner>
+                {
+                    new PropertyOwner
+                    {
+                        ownerId = Guid.NewGuid(),
+                        ownerName = "ownerName 1",
+                        email = "email for PropertyOwner 1"
+                    },
+                    new PropertyOwner
+                    {
+                        ownerId = Guid.NewGuid(),
+                        ownerName = "ownerName 2",
+                        email = "email for PropertyOwner 2"
+                    }
+                };
+        }
     }
 }
