b5d13897238736fd9d5f28ad7aeffcc5c1460b55
hamady2026 <hamady106lethe@gmail.com>
2025-05-23 19:13:46 +0200

    تم ربط زر الاضافه والتعديل للانتقال الي صفحه اللتي تتم فيها العمليه و انشاء داله الحذف المالك كل هذه العمليات تحدث في الذاكره الي ان يتم الربط معا قاعده البيانات

--- Project2025/Components/Pages/PropertyOwnerComponents/PropertyOwnerDesignServices.cs
+++ Project2025/Components/Pages/PropertyOwnerComponents/PropertyOwnerDesignServices.cs
@@ -21,6 +21,37 @@ namespace Project2025.Components.Pages.PropertyOwnerComponents
                         email = "email for PropertyOwner 2"
                     }
                 };
+        }//جيب جميع مالك   //
+
+        public PropertyOwner GetPropertyOwnerByownerId(Guid ownerId)//جيب مالك باستحدام ID //
+        {
+
+            return new PropertyOwner
+
+            {
+                ownerId = ownerId,
+                ownerName = "ownerName 1",
+                email = "email for PropertyOwner 1"
+            };
         }
+
+        public PropertyOwner Save(PropertyOwner propertyOwner)
+        {
+            return propertyOwner;
+
+        }// احفظ مالك//
+
+        public void Delete(PropertyOwner propertyOwner)  //// احذف مالك//
+
+        {
+
+        }
+
+        public PropertyOwner Update(PropertyOwner propertyOwner)
+        {
+            return propertyOwner;
+        }//// تحديث مالك//
     }
 }
+
+
